7ef1e5647e48d066c9b8f7deb8017d6472d21ab5
AgusCF <acardetti295@alumnos.iua.edu.ar>
2024-08-22 11:05:35 -0300

    Agregue potencia

--- Recursividad/Java/src/mains/App.java
+++ Recursividad/Java/src/mains/App.java
@@ -4,15 +4,21 @@ import utilitis.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //--------------------------------EJE 1
         factorial facti = new factorial();
         int resu = 0; 
         resu = facti.facto(5);
         System.out.println("El resultado es " + resu);
-        //--------------------------------
-        int suma=0;
+        //--------------------------------EJE 2
         SumaNumNaturales sumNatural=new SumaNumNaturales();
-        suma=sumNatural.sum(5);
-        System.out.println("La suma da:"+suma);
+        resu=sumNatural.sum(5);
+        System.out.println("La suma da:"+resu);
+        //--------------------------------EJE 5
+        Potencia pot = new Potencia();
+        resu=pot.potenca(2,3);
+        System.out.println("La potencia da:"+resu);
+
+
 
     }
 }
