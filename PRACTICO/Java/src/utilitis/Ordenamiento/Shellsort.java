1c1845ae87484b266d9aad6b089048d1f911b642
Tu Nombre <asdsad@dsa.com>
2024-08-29 11:11:20 -0300

    8

--- PRACTICO/Java/src/utilitis/Ordenamiento/Shellsort.java
+++ PRACTICO/Java/src/utilitis/Ordenamiento/Shellsort.java
@@ -1,5 +1,20 @@
 package utilitis.Ordenamiento;
 
+import java.util.List;
+
 public class Shellsort {
+    public void shellsortOrder(List<Pedido> listaDePedidos){
+        int i = 0;
+        int j = 0;
+        int aux = 0;
+        int gap = 0; 
+        for (gap = listaDePedidos.size()/2; gap > 0; gap/=2) {
+            for (i = gap; i < listaDePedidos.size(); i++) {
+                for (j = 0; j < 10; j++) {
+                    
+                }
+            }
+        };
+    }
 
 }
