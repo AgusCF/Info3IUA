ed4c3d91accaa04361b4ce01fe0a07d7afc4620d
AgusCF <acardetti295@alumnos.iua.edu.ar>
2024-08-29 10:35:44 -0300

    Clase incersion subida
    No se si anda!

--- PRACTICO/Java/src/utilitis/Ordenamiento/Incercion.java
+++ PRACTICO/Java/src/utilitis/Ordenamiento/Incercion.java
@@ -2,14 +2,35 @@ package utilitis.Ordenamiento;
 import java.util.List;
 
 public class Incercion {
-    void IncercionV1(List<Pedido> pedidos){
-        int temp;
-        int posicion;
-        for(int i=0;i<pedidos.size();i++){
-
+    void IncercionV1(List<Pedido> pedidos){//Vercion armada por la clase
+        Pedido temp;
+        int j;
+        for(int i = 1; i < pedidos.size(); i++) {  // Comenzamos desde el segundo elemento
+            j = i;
+            while (j > 0 && pedidos.get(j).getTiempo() < pedidos.get(j-1).getTiempo()) {
+                // Intercambio de elementos
+                temp = pedidos.get(j);
+                pedidos.set(j, pedidos.get(j-1));
+                pedidos.set(j-1, temp);
+                j--;
         }
     }
-    void IncercionV2(List<Pedido>){//Vercion optimizada de Incercion(La del ppt)
+    }
+    void IncercionV2(List<Pedido> pedidos){//Vercion optimizada de Incercion(La del ppt)
+        // Recorremos la lista comenzando desde el segundo elemento
+        for (int i = 1; i < pedidos.size(); i++) {
+            Pedido temp = pedidos.get(i);  // Guardamos el pedido actual en temp
+            int j = i - 1;
 
+            // Comparar el tiempo del pedido actual con los pedidos anteriores
+            while (j >= 0 && pedidos.get(j).getTiempo() > temp.getTiempo()) {
+                // Si el pedido en la posición j tiene un tiempo mayor, lo movemos una posición hacia adelante
+                pedidos.set(j + 1, pedidos.get(j));
+                j--;
+            }
+
+            // Colocamos el pedido de temp en su posición correcta
+            pedidos.set(j + 1, temp);
+        }
     }
 }
