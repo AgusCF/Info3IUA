237a60e64f7e1e2edc4f0ea37277513ce8bfbb90
Santiago Sanchez <ssanchez321@alumnos.iua.edu.ar>
2024-09-05 09:50:28 -0300

    agregacion de Cola

--- PRACTICO/Java/src/utilitis/PilaYCola/Cola.java
+++ PRACTICO/Java/src/utilitis/PilaYCola/Cola.java
@@ -1,5 +1,52 @@
 package utilitis.PilaYCola;
 
-public class Cola {
+import java.util.Scanner;
 
+import utilitis.Ordenamiento.Pedido;
+
+public class Cola {
+    Scanner entrada = new Scanner(System.in);
+    private Pedido[] pedidos = new Pedido[20];
+
+    private int size = 0;
+    private int back = -1, front = 0;
+    private int idSum = 0;
+    private int contador = 0;
+
+    public void enqueue() {
+
+        Pedido newPedido = new Pedido();
+        System.out.println("Ingresa los datos a continuacion:");
+        System.out.print("\nCliente (Nombre): ");
+        if (idSum != 1) {
+            entrada.nextLine();
+        }
+        newPedido.setNombreCliente(entrada.nextLine());
+
+        System.out.print("\nIngresa el tiempo de preparacion(minutos): ");
+        newPedido.setTiempo(entrada.nextInt());
+
+        System.out.print("\nIngresa el precio: ");
+        newPedido.setPrecio(entrada.nextFloat());
+
+        newPedido.setPedido(idSum++);
+
+        if (size < 20) {
+            back = (back + 1) % 20;
+            pedidos[back] = newPedido;
+            size++;
+        } else {
+            System.out.println("La cola está llena.");
+        }
+
+    }
+
+    public void dequeue() {
+        if (size > 0) {
+            front = (front + 1) % 20;
+            size--;
+        } else {
+            System.out.println("La cola está vacía.");
+        }
+    }
 }
