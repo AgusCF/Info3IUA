f0e2c7b58a8c4ea62de62f0986b9d80fb73d7d8b
AgusCF <acardetti295@alumnos.iua.edu.ar>
2024-09-05 10:51:00 -0300

    Controlar que le entra a la funcion

--- PRACTICO/Java/src/utilitis/PilaYCola/Pila.java
+++ PRACTICO/Java/src/utilitis/PilaYCola/Pila.java
@@ -1,45 +1,51 @@
 package utilitis.PilaYCola;
 
 import java.util.Stack;
-
-import utilitis.Ordenamiento.CargDeFun;
 import utilitis.Ordenamiento.Pedido;
 
 public class Pila {
     private int TAM=20;
     private Pedido [] pilaPedido = new Pedido[20] ;
     private int top=-1;
-    //Funcion para saber si etsta vacio o no la PILA
+
+    public Pila(){
+    }
+    //Funciones de control-------------------------
     public boolean isEmpty(int top){
         return(top==-1);
     }
+    public boolean isFull(int top){//Pregunte y me dijieron si
+        return(top==(TAM-1));
+    }
+    //Fin funciones de control---------------------
 
     public void push(Pedido[] cola){
-        pilaPedido[++top] = cola[10];
-    }
-
-    
-    public void pop(){
-        if (funcion.itsEmpty()) {
-           System.out.println("No se puede realizar el POP en la Pila porque esta vacia");
-        } else {
-        }
-    }
-    public void top(){
-        if (funcion.itsEmpty()) {
-            System.out.println("No se puede mostrar el TOP en la Pila porque esta vacia");
-        } else {
-            //System.out.println(cola.toString());
+        if(isFull(top)){
+            System.out.println("No se puede realizar el PUSH en la Pila porque esta llena");
+        }else{
+           pilaPedido[++top] = cola[10]; //Ver que onda el tema cola
         }
         
     }
 
-    public void makeEmpty(){
-        if (itEmpty(top)) {
-            System.out.println("No se puede 'MAKE  EMPTY' de la Pila porque esta vacia");
-        } else {
-            //System.out.println(cola.toString());
+    
+    public void pop(){
+        if(isEmpty(top)){
+            System.out.println("No se puede realizar el POP en la Pila porque esta vacia");
+        }else{
+            top--;
         }
+    }
+    public void topStak(){
+        if(isEmpty(top)){
+            System.out.println("No se puede mostrar el TOP en la Pila porque esta vacia");
+        }else{
+            System.out.println("El ultimo pedido completado es: " + pilaPedido[top].getNombreCliente());
+        }
+    }
+
+    public void makeEmpty(){
+        top=-1;
     }
     
 }
